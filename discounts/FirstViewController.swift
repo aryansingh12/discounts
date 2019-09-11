c730d69004fffad0e5f4c952a42e3c2826bb123f
aryansingh12 <aryansingh@umass.edu>
2019-09-10 21:31:14 -0400

    Initial Commit

---
+++ discounts/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  discounts
+//
+//  Created by atcdev on 9/10/19.
+//  Copyright © 2019 Aryan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ discounts/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  discounts
+//
+//  Created by atcdev on 9/10/19.
+//  Copyright © 2019 Aryan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
